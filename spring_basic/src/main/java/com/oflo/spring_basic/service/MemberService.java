8299dc9b8cbf9af04e3ec0969ea5eee7a2a12869
Oflo <57768558+mmkser10@users.noreply.github.com>
2021-03-20 19:41:44 +0900

    feat : MemberService와 테스트케이스 구현

---
+++ spring_basic/src/main/java/com/oflo/spring_basic/service/MemberService.java
@@ -0,0 +1,38 @@
+package com.oflo.spring_basic.service;
+
+import com.oflo.spring_basic.domain.Member;
+import com.oflo.spring_basic.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemoryMemberRepository memberRepository;
+
+    public MemberService(MemoryMemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
